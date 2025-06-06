fca0e98223307128beb6e088f2355e019c99ff25
Salih Aslantas <salihcode@gmail.com>
2025-06-06 08:12:38 +0300

    foundation was constructed

--- app/src/main/java/com/example/running/MainActivity.kt
+++ app/src/main/java/com/example/running/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.running
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.running.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.submit.setOnClickListener {
+            val CircularTrack = binding.circularTrack.isActivated
+            val meter = binding.meterEditText.text.toString().toDouble()
+            if (meter != 0.0) {
+                val intent = Intent(this@MainActivity, RunningActivity::class.java)
+                intent.putExtra("CircularTrack",CircularTrack)
+                intent.putExtra("meter", meter)
+                startActivity(intent)
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/running/RunningActivity.kt
@@ -0,0 +1,48 @@
+package com.example.running
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.running.databinding.ActivityRunningBinding
+import kotlin.math.roundToInt
+
+class RunningActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRunningBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityRunningBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val circularTrack = intent.getBooleanExtra("CircularTrack", false)
+        val meter = intent.getDoubleExtra("meter",0.0)
+        var distance = 0.0
+        binding.lap.setOnClickListener {
+            distance += if (circularTrack) {
+                meter * 2
+            } else {
+                meter
+            }
+            binding.kilometer.text = "Kilometer: " + (distance / 1000).toString()
+            binding.Calories.text = "Calories: " + calculateCalories((distance / 1000), 70.0).toString()
+        }
+    }
+    fun calculateCalories(distance: Double, weight: Double, pace: String = "moderate"): Int {
+        val metValues = mapOf(
+            "slow" to 8,
+            "moderate" to 10,
+            "fast" to 12,
+            "very_fast" to 15
+        )
+
+        val met = metValues[pace] ?: 10
+        return (met * weight * distance * 0.1).roundToInt()
+    }
+}
